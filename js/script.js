3becf884830aefc54c1f6d1321a2c4943b5723bb
Greg <vikodin55@yandex.ru>
2020-02-17 08:08:59 +0600

    123456
    1234

--- js/script.js
+++ js/script.js
@@ -16,4 +16,19 @@ $(document).ready(function(){
             }
         ]
       });
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content')
+          .removeClass('catalog__content_active')
+          .eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      $('.catalog-item__link').each(function(i) {
+          $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').toggleClass('catalog-item__content_active');
+          })
+      })
   });
