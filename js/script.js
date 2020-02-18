bcca7fa066619a24feb31784284421704dfcf4c7
Greg <vikodin55@yandex.ru>
2020-02-18 20:51:22 +0600

    30commit

--- js/script.js
+++ js/script.js
@@ -98,4 +98,17 @@ $(document).ready(function(){
         });
         return false;
     });
+    //smooth scrool and pageup
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
